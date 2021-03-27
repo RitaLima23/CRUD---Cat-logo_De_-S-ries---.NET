69cc49471874f7d5c2358b8d44f7047e771e56cd
RitaLima23 <cassialimacoelho18@gmail.com>
2021-03-26 23:52:15 -0300

    Terceiro commit

---
+++ Enum/Plataformas.cs
@@ -0,0 +1,11 @@
+namespace DIO.Series
+{
+    public enum Plataformas{
+        Amazon = 1,
+        Netflix = 2,
+        Disney = 3,
+        Globo = 4,
+        Telecine = 5,
+        HBO = 6
+    }
+}
